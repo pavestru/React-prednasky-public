64f1cca0b8cdcbc0b013f43d2f4bd2ea64279fe6
Pavel Struhar <pavel.struhar@accenture.com>
2021-04-26 14:05:29 +0200

    Add UNDO to frontend

--- todo-list/src/components/TodoForm.js
+++ todo-list/src/components/TodoForm.js
@@ -20,10 +20,10 @@ const TodoForm = (props) => {
     <div className="todo-form">
       <h4>Add TODO</h4>
       <form onSubmit={onFormSubmit}>
-        <input className="form-input" type="text" name="newtodo" value={name} onChange={(e) => setName(e.target.value)} placeholder="Co budes robit?"/>
+        <input required className="form-input" type="text" name="newtodo" value={name} onChange={(e) => setName(e.target.value)} placeholder="Co budes robit?"/>
         <textarea className="form-text" value={description} onChange={(e) => setDescription(e.target.value)} placeholder="...pridaj par detailov"></textarea>
-        <input className="form-input" type="date" value={dueDate} onChange={(e) => setDueDate(e.target.value)} />
-        <button className="button button-add">Add TODO</button>      
+        <input required className="form-input" type="date" value={dueDate} onChange={(e) => setDueDate(e.target.value)} />
+        <button className="button button-add" disabled={!name || !dueDate}>Add TODO</button>      
       </form>
     </div>
   )
