f048c83e93757c23c209f5430a38cb4844bfe0ea
Pavel Struhar <pavel.struhar@accenture.com>
2021-04-28 11:58:16 +0200

    Add Dialog

--- todo-list/src/components/TodoList.js
+++ todo-list/src/components/TodoList.js
@@ -1,4 +1,12 @@
 import React from 'react'
+
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+
 import { Todo }  from './Todo'
 import './TodoList.css';
 
@@ -6,6 +14,21 @@ let ID = 1;
 const getId = () => ID++;
 
 const TodoList = (props) => {
+  const [openDialog, setOpenDialog] = React.useState(false);
+
+  const handleClickOpenDialog = () => {
+    setOpenDialog(true);
+  };
+
+  const handleCloseDialog = () => {
+    setOpenDialog(false);
+  };
+
+  const handleDelete = () => {
+    props.onRemoveAll();
+    handleCloseDialog();
+  }
+
   const todos = props.todos;
   if (todos.length <= 0) {
     return (<div className="todo-nothing"><p>Nothing to do</p></div>);
@@ -14,7 +37,7 @@ const TodoList = (props) => {
     <div className="todo-list">
       <div className="todo-header">
         <div>Total number of tasks: {todos.length}</div>
-        <button className="button" onClick={() => props.onRemoveAll()}>Remove all</button>
+        <button className="button" onClick={handleClickOpenDialog}>Remove all</button>
       </div>
       {todos.map((todo) => {
         return (
@@ -29,6 +52,27 @@ const TodoList = (props) => {
           </div>
         );
       })}
+      <Dialog
+        open={openDialog}
+        onClose={handleCloseDialog}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">Are you sure you want to delete all ToDo items?</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            Deleting all todo items cannot be reverted.
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleCloseDialog} color="primary">
+            Cancel
+          </Button>
+          <Button onClick={handleDelete} color="primary" autoFocus>
+            Delete
+          </Button>
+        </DialogActions>
+      </Dialog>
     </div>
   )
 }
